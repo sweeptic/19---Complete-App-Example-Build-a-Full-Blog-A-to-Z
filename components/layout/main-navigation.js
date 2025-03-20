48b41d8f4ad2f082f6bc5f945f58818e86339dd0
Attila Horvath <sweeptic@gmail.com>
2025-03-20 14:35:33 +0100

    005 Adding Layout & Navigation

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import Logo from './logo';
+
+export default function MainNavigation() {
+  return (
+    <header>
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}

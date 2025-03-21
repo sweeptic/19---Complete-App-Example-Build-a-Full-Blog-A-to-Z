74dd88097774506451695ba6d03f55073293f222
Attila Horvath <sweeptic@gmail.com>
2025-03-21 12:06:38 +0100

    012 Rendering Markdown As JSX

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,6 +1,7 @@
 import { DUMMY_POSTS } from '../../../pages';
 import classes from './post-content.module.css';
 import PostHeaderPage from './post-header';
+import ReactMarkdown from 'react-markdown';
 
 const DUMMY_POST_ONE = { ...DUMMY_POSTS[0], content: '# This is a first post' };
 
@@ -12,7 +13,7 @@ export default function PostContentPage() {
   return (
     <article className={classes.content}>
       <PostHeaderPage title={DUMMY_POST_ONE.title} image={imagePath} />
-      {DUMMY_POST_ONE.content}
+      <ReactMarkdown>{DUMMY_POST_ONE.content}</ReactMarkdown>
     </article>
   );
 }

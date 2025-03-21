8a1e07196428e4cf73e35a3189653251ebaf9d46
Attila Horvath <sweeptic@gmail.com>
2025-03-21 09:58:47 +0100

    008 Adding A Post Grid & Post Items

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,5 +1,24 @@
+import Image from 'next/image';
 import classes from './posts-item.module.css';
 
-export default function PostItem() {
-  return <div>PostItem</div>;
+export default function PostItem(props) {
+  const { title, image, excerpt, date, slug } = props.post;
+
+  const readableDate = new Date(date).toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
+  const imagePath = `/images/posts/${slug}/${image}`;
+
+  return (
+    <li className={classes.post}>
+      <Link>
+        <div className={classes.image}>
+          <Image src={imagePath} alt={title} width={300} height={200} />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <time datetime="">{readableDate}</time>
+          <p>{excerpt}</p>
+        </div>
+      </Link>
+    </li>
+  );
 }
